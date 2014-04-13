b01ca0c44dea87227f676982f2695e171fd18843
David McMahon <davidmcmahon@Davids-MacBook-Pro.local>
2014-04-13 09:51:01 -0600

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by David McMahon on 4/13/14.
+//  Copyright (c) 2014 Bluewater. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by David McMahon on 4/13/14.
+//  Copyright (c) 2014 Bluewater. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
